448f9ccaf492fc19a943ff799d1c014941b618d2
小造xu_zh <ngc7331@outlook.com>
2023-02-18 13:28:10 +0000

    feat: build web

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ See official repo [here](https://github.com/MCSManager/MCSManager)
 2. Set DOCKERHUB_USER and DOCKERHUB_PASS in your env, E.g `export DOCKERHUB_USER=xxx`
 3. Run `build-all.sh`
 
-### Run
+### Run Daemon
 ```
 $ docker run -d --name mcsm-daemon \
              -p 24444:24444 \
@@ -27,7 +27,7 @@ $ docker run -d --name mcsm-daemon \
 Notes:
 1. Replace `your/path/to/xxx` with your actual path
 2. Replace `<tag>` with `latest` or any valid tag, checkout [tags](https://hub.docker.com/repository/docker/ngc7331/mcsmanager-daemon/tags) on Docker Hub
-3. If you don't want use mcsm to control docker, delete `-v /var/run/docker.sock:/var/run/docker.sock \`
+3. If you don't want use mcsm to control docker, remove `-v /var/run/docker.sock:/var/run/docker.sock \`
 
 ### Stop & Remove
 ```
--- build-all.sh
+++ build-all.sh
@@ -4,20 +4,35 @@ if [[ -z $DOCKERHUB_USER || -z $DOCKERHUB_PASS ]]; then
     echo "Please set DOCKERHUB_USER and DOCKERHUB_PASS in your env" && exit 1
 fi
 
+# daemon
 JDK_VERSIONS="8 11 17"
-MCSM_VERSION=$(curl "https://raw.githubusercontent.com/MCSManager/MCSManager-Daemon-Production/master/package.json" | jq -r ".version")
+MCSM_DAEMON_VERSION=$(curl "https://raw.githubusercontent.com/MCSManager/MCSManager-Daemon-Production/master/package.json" | jq -r ".version")
 
-# build
 for i in $JDK_VERSIONS; do
+    echo "=== Building mcsmanager-daemon:$MCSM_DAEMON_VERSION-jdk$i"
+
     docker build -f ./dockerfile-daemon \
-                 -t $DOCKERHUB_USER/mcsmanager-daemon:$MCSM_VERSION-jdk$i \
+                 -t $DOCKERHUB_USER/mcsmanager-daemon:$MCSM_DAEMON_VERSION-jdk$i \
                  -t $DOCKERHUB_USER/mcsmanager-daemon:latest-jdk$i \
                  --build-arg JDK_VERSION=$i \
-                 . || echo "Build FAILED"
+                 . || echo "=== Build Failed ==="
 done
 
 docker tag $DOCKERHUB_USER/mcsmanager-daemon:latest-jdk17 $DOCKERHUB_USER/mcsmanager-daemon:latest
 
+# web
+MCSM_WEB_VERSION=$(curl "https://raw.githubusercontent.com/MCSManager/MCSManager-Web-Production/master/package.json" | jq -r ".version")
+
+echo "=== Building mcsmanager-web:$MCSM_WEB_VERSION"
+
+docker build -f ./dockerfile-web \
+             -t $DOCKERHUB_USER/mcsmanager-web:$MCSM_WEB_VERSION \
+             -t $DOCKERHUB_USER/mcsmanager-web:latest \
+             . || echo "=== Build Failed ==="
+
 # push
+echo "=== Pushing to Docker Hub ==="
+
 echo $DOCKERHUB_PASS | docker login --username $DOCKERHUB_USER --password-stdin
 docker push -a $DOCKERHUB_USER/mcsmanager-daemon
+docker push -a $DOCKERHUB_USER/mcsmanager-web
