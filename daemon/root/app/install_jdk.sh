45a69eb6dc412c03565d8306958924947f866f5e
ngc7331 <ngc7331@outlook.com>
2024-09-11 18:18:31 +0800

    feat: add java${VERSION} aliases

--- daemon/root/app/install_jdk.sh
+++ daemon/root/app/install_jdk.sh
@@ -6,11 +6,9 @@ VERSION=$1
 SUFFIX="-headless"
 
 if [ -z "${VERSION}" ]; then
-  echo "Skipping JDK installation..."
   exit 0
 fi
 
-echo "Installing JDK ${VERSION}..."
 if [ "${VERSION}" = "8" ]; then
   SUFFIX=""
 elif [ "${VERSION}" = "22" ]; then
@@ -19,4 +17,8 @@ elif [ "${VERSION}" = "22" ]; then
 fi
 
 apk add --no-cache openjdk${VERSION}-jre${SUFFIX}
-java -version
+
+ln -s /usr/lib/jvm/java-${VERSION}-openjdk/bin/java /usr/bin/java${VERSION}
+
+# sanity check
+java${VERSION} -version
