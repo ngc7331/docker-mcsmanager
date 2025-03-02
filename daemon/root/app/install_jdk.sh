fad5f8edd213bda318b6b078374cdcdf9590dea3
ngc7331 <ngc7331@outlook.com>
2025-03-02 14:15:13 +0800

    update base image to 3.21

--- daemon/root/app/install_jdk.sh
+++ daemon/root/app/install_jdk.sh
@@ -11,9 +11,9 @@ fi
 
 if [ "${VERSION}" = "8" ]; then
   SUFFIX=""
-elif [ "${VERSION}" = "22" ]; then
-  echo "Warning: openjdk22 is not stable yet, use with caution"
-  sed -n "s;v3.20/main;edge/testing;p" /etc/apk/repositories >> /etc/apk/repositories
+elif [ "${VERSION}" = "22" ] || [ "${VERSION}" = "23" ]; then
+  echo "Warning: openjdk22/23 is not stable yet, use with caution"
+  sed -n "s;v3.21/main;edge/testing;p" /etc/apk/repositories >> /etc/apk/repositories
 fi
 
 apk add --no-cache openjdk${VERSION}-jre${SUFFIX}
