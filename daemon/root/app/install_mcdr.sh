b829bd7cfa2ea6bd93cf8d4719919733efe15f18
ngc7331 <ngc7331@outlook.com>
2025-04-18 22:11:34 +0800

    fix(mcdr): use venv

--- daemon/root/app/install_mcdr.sh
+++ daemon/root/app/install_mcdr.sh
@@ -2,11 +2,29 @@
 
 set -e
 
-# ensure deps are installed
-apk add --no-cache python3 py3-pip py3-psutil
+if [ -z "$(which python3)" ]; then
+  apk add --no-cache python3 py3-pip py3-psutil
+fi
+
+VENV_PATH="${MCSM_PATH}/daemon/data/venv/mcdreforged"
+PYTHON="${VENV_PATH}/bin/python3"
 
 # install mcdr
-# break system dependencies
-python3 -m pip install --no-cache-dir --break-system-packages mcdreforged
+if [ ! -f "${PYTHON}" ]; then
+  echo "Creating virtualenv for MCDR..."
+  python3 -m venv --system-site-packages "${VENV_PATH}"
+fi
+
+${PYTHON} -m pip install --upgrade mcdreforged
+
+chown abc:abc "${VENV_PATH}"
+
+# Create alias
+cat << EOF > /usr/bin/mcdreforged
+#!/bin/bash
+${PYTHON} -m mcdreforged "\$@"
+EOF
+
+chmod +x /usr/bin/mcdreforged
 
 mcdreforged --version
